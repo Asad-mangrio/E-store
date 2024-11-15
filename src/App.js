9dcf3b50f1c6fed50b253c8bddaa641ef48926fe
Asad <you@example.comxwaveasadmangrio@gmail.com>
2024-11-15 12:06:00 +0500

    finesh

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import DrawerAppBar from './Components/Layout/Layout';
 import { Productdata } from './Components/Productdata/Productdata';
+import Signin from './Components/outh/Signin/Signin';
+import Signup from './Components/outh/Signup/Signup';
+import Errorpage from './Components/ErrorPage/Errorpage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 
 
 function App() {
+  const router = createBrowserRouter([
+    {
+     path:"/",element :  < DrawerAppBar/>,errorElement:<Errorpage/>,
+    },
+   
+    {
+     path:"/Signin",element : <Signin/>,
+    },
+    {
+     path:"/Signup",element :   < Signup/>,
+    }
+  ])
   return (
     <div className="App">
-     < DrawerAppBar/>
-     <Productdata/>
+    
+     
+     <RouterProvider router={router} />
     </div>
   );
 }
